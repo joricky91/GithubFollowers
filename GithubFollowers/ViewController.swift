5c31c19ae5cd9e13aedc5e300c7e7628321ea7c9
joricky91 <jonathanricky91@gmail.com>
2024-06-10 13:55:10 +0700

    Initial Commit

---
+++ GithubFollowers/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GithubFollowers
+//
+//  Created by Jonathan Ricky Sandjaja on 10/06/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = UIColor.cyan
+    }
+
+
+}
+
