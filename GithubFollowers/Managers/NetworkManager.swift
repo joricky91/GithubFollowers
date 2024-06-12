bcc19be94cf385b118f49714c427ced12ceed084
joricky91 <jonathanricky91@gmail.com>
2024-06-12 15:58:52 +0700

    feat: finishing network manager

---
+++ GithubFollowers/Managers/NetworkManager.swift
@@ -0,0 +1,55 @@
+//
+//  NetworkManager.swift
+//  GithubFollowers
+//
+//  Created by Jonathan Ricky Sandjaja on 12/06/24.
+//
+
+import Foundation
+
+class NetworkManager {
+    
+    static let shared = NetworkManager()
+    let baseURL = "https://api.github.com/users/"
+    
+    private init() {}
+    
+    func getFollowers(for username: String,
+                      page: Int,
+                      completionHandler: @escaping ([Follower]?, ErrorMessage?) -> Void) {
+        let endpoint = baseURL + "\(username)/followers?per_page=100&page=\(page)"
+        
+        guard let url = URL(string: endpoint) else {
+            completionHandler(nil, .invalidUsername)
+            return
+        }
+        
+        let task = URLSession.shared.dataTask(with: url) { data, response, error in
+            if let _ = error {
+                completionHandler(nil, .unableToComplete)
+                return
+            }
+            
+            guard let response = response as? HTTPURLResponse, response.statusCode == 200 else {
+                completionHandler(nil, .invalidResponse)
+                return
+            }
+            
+            guard let data = data else {
+                completionHandler(nil, .invalidData)
+                return
+            }
+            
+            do {
+                let decoder = JSONDecoder()
+                decoder.keyDecodingStrategy = .convertFromSnakeCase
+                let followers = try decoder.decode([Follower].self, from: data)
+                completionHandler(followers, nil)
+            } catch {
+                completionHandler(nil, .invalidData)
+            }
+        }
+        
+        task.resume()
+    }
+}
--- GithubFollowers/Screens/FollowerListViewController.swift
+++ GithubFollowers/Screens/FollowerListViewController.swift
@@ -22,6 +22,16 @@ class FollowerListViewController: UIViewController {
         
         view.backgroundColor = .systemBackground
         navigationController?.navigationBar.prefersLargeTitles = true
+        
+        NetworkManager.shared.getFollowers(for: username, page: 1) { [weak self] followers, errorMessage in
+            guard let self, let followers else {
+                self?.presentGFAlertOnMainThread(title: "Bad Stuff Happened", message: errorMessage?.rawValue ?? "", buttonTitle: "Ok")
+                return
+            }
+            
+            print("Follower count = \(followers.count)")
+            print("Followers: \(followers)")
+        }
     }
 
 }
