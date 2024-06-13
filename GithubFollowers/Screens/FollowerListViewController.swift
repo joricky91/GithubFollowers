c2ff2ab71c527b41fa34078b5642afc483c2d132
joricky91 <jonathanricky91@gmail.com>
2024-06-13 17:11:09 +0700

    feat: finishing collection view and download image

--- GithubFollowers/Screens/FollowerListViewController.swift
+++ GithubFollowers/Screens/FollowerListViewController.swift
@@ -9,9 +9,15 @@ import UIKit
 
 class FollowerListViewController: UIViewController {
     
+    enum Section {
+        case main
+    }
+    
     var username: String = ""
+    var followers: [Follower] = []
     
     var collectionView: UICollectionView!
+    var dataSource: UICollectionViewDiffableDataSource<Section, Follower>!
     
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
@@ -25,6 +31,7 @@ class FollowerListViewController: UIViewController {
         configureCollectionView()
         configureViewController()
         getFollowers()
+        configureDataSource()
     }
     
     func configureViewController() {
@@ -33,22 +40,42 @@ class FollowerListViewController: UIViewController {
     }
     
     func configureCollectionView() {
-        collectionView = UICollectionView(frame: view.bounds, collectionViewLayout: UICollectionViewLayout())
+        collectionView = UICollectionView(frame: view.bounds, collectionViewLayout: UIHelper.createThreeColumnFlowLayout(in: view))
         view.addSubview(collectionView)
         
-        collectionView.backgroundColor = .systemPink
+        collectionView.backgroundColor = .systemBackground
         collectionView.register(FollowerCell.self, forCellWithReuseIdentifier: FollowerCell.reuseID)
     }
     
     func getFollowers() {
         NetworkManager.shared.getFollowers(for: username, page: 1) { [weak self] result in
+            guard let self else { return }
+            
             switch result {
             case .success(let followers):
-                print("Follower count = \(followers.count)")
-                print("Followers: \(followers)")
+                self.followers = followers
+                self.updateData()
             case .failure(let error):
-                self?.presentGFAlertOnMainThread(title: "Bad Stuff Happened", message: error.rawValue, buttonTitle: "Ok")
+                self.presentGFAlertOnMainThread(title: "Bad Stuff Happened", message: error.rawValue, buttonTitle: "Ok")
             }
+        }
+    }
+    
+    func configureDataSource() {
+        dataSource = UICollectionViewDiffableDataSource<Section, Follower>(collectionView: collectionView, cellProvider: { collectionView, indexPath, follower in
+            guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: FollowerCell.reuseID, for: indexPath) as? FollowerCell else { return UICollectionViewCell() }
+            cell.set(follower: follower)
+            return cell
+        })
+    }
+    
+    func updateData() {
+        var snapshot = NSDiffableDataSourceSnapshot<Section, Follower>()
+        snapshot.appendSections([.main])
+        snapshot.appendItems(followers)
+        
+        DispatchQueue.main.async {
+            self.dataSource.apply(snapshot, animatingDifferences: true)
         }
     }
 
