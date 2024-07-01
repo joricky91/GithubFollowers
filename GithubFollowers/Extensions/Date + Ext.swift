45a48d29efe9a49bc2cc612f30614ab8858f8147
joricky91 <jonathanricky91@gmail.com>
2024-07-01 10:52:13 +0700

    feat: finished followers and repos component and display

---
+++ GithubFollowers/Extensions/Date + Ext.swift
@@ -0,0 +1,19 @@
+//
+//  Date + Ext.swift
+//  GithubFollowers
+//
+//  Created by Jonathan Ricky Sandjaja on 28/06/24.
+//
+
+import Foundation
+
+extension Date {
+    
+    func convertToMonthYearFormat() -> String {
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = "MMM yyyy"
+        
+        return dateFormatter.string(from: self)
+    }
+    
+}
---
+++ GithubFollowers/Extensions/String + Ext.swift
@@ -0,0 +1,26 @@
+//
+//  String + Ext.swift
+//  GithubFollowers
+//
+//  Created by Jonathan Ricky Sandjaja on 28/06/24.
+//
+
+import Foundation
+
+extension String {
+    
+    func convertToDate() -> Date? {
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = "yyyy-MM-dd'T'HH:mm:ssZ"
+        dateFormatter.locale = Locale(identifier: "id")
+        dateFormatter.timeZone = .current
+        
+        return dateFormatter.date(from: self)
+    }
+    
+    func convertToDisplayFormat() -> String {
+        guard let date = self.convertToDate() else { return "N/A" }
+        return date.convertToMonthYearFormat()
+    }
+    
+}
