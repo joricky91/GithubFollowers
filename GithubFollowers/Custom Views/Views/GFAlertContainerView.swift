5f8c3f222233c5496906a7c3bf7ffab0a5844ebf
joricky91 <jonathanricky91@gmail.com>
2024-07-25 15:24:29 +0700

    feat: refactor GF Alert

---
+++ GithubFollowers/Custom Views/Views/GFAlertContainerView.swift
@@ -0,0 +1,30 @@
+//
+//  GFAlertContainerView.swift
+//  GithubFollowers
+//
+//  Created by Jonathan Ricky Sandjaja on 25/07/24.
+//
+
+import UIKit
+
+class GFAlertContainerView: UIView {
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        configure()
+    }
+    
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+    }
+    
+    private func configure() {
+        backgroundColor = .systemBackground
+        layer.cornerRadius = 16
+        layer.borderWidth = 2
+        layer.borderColor = UIColor.white.cgColor
+        translatesAutoresizingMaskIntoConstraints = false
+    }
+
+}
