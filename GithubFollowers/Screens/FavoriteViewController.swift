8003893059d52a8b7e1d9c6099c45d7f2f21ccd8
joricky91 <jonathanricky91@gmail.com>
2024-08-20 16:35:52 +0700

    fix: fix empty state not appearing after deleting favorites and is empty

--- GithubFollowers/Screens/FavoriteViewController.swift
+++ GithubFollowers/Screens/FavoriteViewController.swift
@@ -99,6 +99,11 @@ extension FavoriteViewController: UITableViewDelegate, UITableViewDataSource {
             guard let error else {
                 self.favorites.remove(at: indexPath.row)
                 tableView.deleteRows(at: [indexPath], with: .left)
+                
+                if self.favorites.isEmpty {
+                    self.showEmptyStateView(with: "No Favorites?\nAdd one on the follower screen", in: self.view)
+                }
+                
                 return
             }
             
